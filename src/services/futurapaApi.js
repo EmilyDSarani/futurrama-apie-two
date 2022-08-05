23d002b612160786cebb730d40f3380ee4916eb4
EmilyDSarani <emily.sarani@gmail.com>
2022-08-05 17:16:22 -0500

    ADD: a plan in the readme and the plan for fetching

---
+++ src/services/futurapaApi.js
@@ -0,0 +1,9 @@
+//first we need to fetch all the characters and export that out
+//we need a response and we need that response to be json
+//then return
+
+
+//then we need to search the characters by their name
+//this is so that we can grab the details page later
+// need the response and the response to be in json
+// just in case there are multiple characets with the same name, put it to sub 0
